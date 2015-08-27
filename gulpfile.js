05d13be23135754a21d3c129c3863f17b5c1ec15
Tyler <tyler@future-ink.com>
2015-08-27 16:44:45 -0700

    gulpfile.js modifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-var dest = "./build";
-var src = './src';
+var dest  = "./build";
+var src   = './src';
 
-var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    csswring = require('csswring'),
-    cssnext = require('cssnext'), //minify css
-    autoprefixer = require('autoprefixer-core'),
-    rucksack = require('rucksack-css'),
-    sourcemaps = require('gulp-sourcemaps'),
-    atImport = require('postcss-import'),
-    url = require('postcss-url');
-
+var gulp          = require('gulp'),
+    postcss       = require('gulp-postcss'),
+    csswring      = require('csswring'),
+    cssnext       = require('cssnext'), //minify css
+    autoprefixer  = require('autoprefixer-core'),
+    rucksack      = require('rucksack-css'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    lost          = require('lost'),
+    atImport      = require('postcss-import'),
+    //url = require('postcss-url');
+    browserSync   = require('browser-sync');
+    
 
 var handleErrors = require('./handleErrors');
 
 gulp.task('styles', function() {
   var processors = [
+    atImport(),
     csswring(),
     autoprefixer({
       browsers:['last 2 version']
@@ -26,23 +29,36 @@ gulp.task('styles', function() {
       customSelectors: true
     }),
     rucksack(),
+    lost()
   ];
 
-  return gulp.src('./src/css/**/*.css')
+  return gulp.src('./src/css/app.css')
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .on('error', handleErrors)
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dest + '/css'));
+    .pipe(gulp.dest(dest + '/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('markup', function() {
   return gulp.src(src + '/htdocs/**/*.html')
-    .pipe(gulp.dest(dest));
+    .pipe(gulp.dest(dest))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      // Serve up our build folder
+      baseDir: dest
+    }
+  });
 });
 
 
-gulp.task('watch', function() {
-  gulp.watch(src + '/css/**/*.css', ['styles']);
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch(src + '/css/app.css', ['styles']);
+  //gulp.watch(src + '/images/**', ['images']);
   gulp.watch(src + '/htdocs/**/*.html', ['markup']);
 });
