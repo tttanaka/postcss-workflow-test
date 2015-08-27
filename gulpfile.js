1be1ed50f39ed8031f2cac0cf68c14959711f527
Tyler <tyler@future-ink.com>
2015-08-26 17:12:03 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp          = require('gulp'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer-core'),
+    rucksack      = require('rucksack-css'),
+    precss        = require('precss'),
+    sourcemaps    = require('gulp-sourcemaps');
+
+gulp.task('css', function () {
+  return gulp.src('./src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+            autoprefixer({ browsers: ['last 3 versions'] }),
+            rucksack(),
+            precss
+          ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/css'));
+});
